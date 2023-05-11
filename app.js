eabf2901d275937cf2851bed7632cb99df289857
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-05-11 15:30:31 +0200

    Refactored code

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use("/users", usersRouter);
+app.use("/api/pets", petsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
