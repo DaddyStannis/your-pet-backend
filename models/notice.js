76becb1f4168b0cec5af4bf495595b755e0bf087
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-05-11 17:15:50 +0200

    Make Notice schema and model
    Refactored code

--- models/notice.js
+++ models/notice.js
@@ -3,11 +3,110 @@ import Joi from "joi";
 import { handleMongooseError } from "../helpers/index.js";
 
 const noticeSchema = new Schema({
+    category: {
+        type: String,
+        enum: ["my pet", "sell", "lost-found", "for-free"],
+        required: [true, "Set category for your pet"]
+    },
+    type: {
+        type: String,
+        required: [true, "Set type of your pet"]
+    },
+    name: {
+        type: String,
+        minLength: 2,
+        maxLength: 16,
+        required: [true, "Set name for your pet"]
+    },
+    birth: {
+        type: Date,
+        default: null
+    },
+    breed: {
+        type: String,
+        minLength: 2,
+        maxLength: 16,
+        required: [true, "Set breed of your pet"]
+    },
+    file: {
+        type: String,
+    },
+    sex: {
+        type: String,
+        enum: ["male", "female"],
+        required: [true, "Set sex of your pet (male or female)"]
+    },
+    location: {
+        type: String,
+        required: [true, "Set the city of your pet. Where your pet is now"]
+    },
+    price: {
+        type: Number
+    },
+    comments: {
+        type: String,
+        minLength: 8,
+        maxLength: 120,
+        default: null
+    },
+    favorite: {
+        type: Boolean,
+        default: false
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: true,
+    }
+}, { versionKey: false })
 
+noticeSchema.post('save', handleMongooseError)
+
+const addNoticeSchema = Joi.object({
+    category: Joi.string().regex(/^(my pet|sell|lost-found|for-free)$/).required().messages({
+        'any.required': 'missing field category'
+    }),
+    type: Joi.string().pattern(/^[A-Za-z ]+$/).required().messages({
+        'any.required': 'missing required type field'
+    }),
+    name: Joi.string().min(2).max(16).required().messages({
+        'any.required': 'missing required name field'
+    }),
+    birth: Joi.number(),
+    breed: Joi.string().min(2).max(16).required().messages({
+        'any.required': 'missing required breed field'
+    }),
+    file: Joi.string(),
+    sex: Joi.string().regex(/^(male|female)$/).required().messages({
+        'any.required': 'missing required sex field'
+    }),
+    location: Joi.string().pattern(/^[A-Za-z ]+$/).when("category", {
+      is: Joi.valid("sell", "lost-found", "for-free"),
+      then: Joi.required(),
+      otherwise: Joi.optional(),
+    }).messages({
+        'any.required': 'missing required location field'
+    }),
+    price: Joi.number().min(0).when("category", {
+      is: "sell",
+      then: Joi.number().min(1).required(),
+      otherwise: Joi.optional(),
+    }).messages({
+        'any.required': 'missing required price field'
+  }),
+    comments: Joi.string().min(8).max(120).regex(/^[\s\S]*.*[^\s][\s\S]*$/),
+    favorite: Joi.boolean()
 })
 
+const patchPetFavoriteSchema = Joi.object({
+    favorite: Joi.boolean().required().messages({
+        'any.required': 'missing field favorite'
+    })
+ })
+
 const schemas = {
-  
+    addNoticeSchema,
+    patchPetFavoriteSchema,
 };
 
 const Notice = model("notice", noticeSchema)
--- routes/api/notices-routes.js
+++ routes/api/notices-routes.js
@@ -7,20 +7,33 @@ import { validateBody } from "../../decorators/index.js";
 
 const router = express.Router();
 
+// створити ендпоінт для пошуку оголошеннь по заголовку
 router.get("/find/byTitle")
 
+// створити ендпоінт для отримання оголошень по категоріям
 router.get("/find/byCategory")
 
+// створити ендпоінт для отримання одного оголошення
 router.get("/find/:noticeId")
 
+// створити ендпоінт для додавання оголошення до обраних
 router.patch("/:noticeId/favorite")
 
+// створити ендпоінт для отримання оголошень авторизованого користувача доданих ним же в обрані
+// створити ендпоінт для видалення оголошення авторизованого користувача доданих цим же до обраних
 router.get("/favorite", authenticate)
 
+// створити ендпоінт для додавання оголошень відповідно до обраної категорії
 router.post("/", authenticate)
 
+// створити ендпоінт для отримання оголошень авторизованого кристувача створених цим же користувачем
 router.get("/:userId", authenticate)
 
+// створити ендпоінт для видалення оголошення авторизованого користувача створеного цим же користувачем 
 router.delete("/:noticeId", authenticate)
+
+// ADDITIONALY I`M NOT SURE THAT WE NEED THIS
+// CREATED ENDPOINT FOR EDIT YOUR NOTICE
+router.put('/:noticeId', authenticate)
 
 export default router;
