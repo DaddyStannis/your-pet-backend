798e680cdf08c176da02033f949bd1f08caa536a
Stanislav Sakun <stanislav.sakun@ukr.net>
2023-05-15 16:05:37 +0300

    Added default props to resizeImg

--- helpers/resizeImg.js
+++ helpers/resizeImg.js
@@ -1,6 +1,6 @@
 import Jimp from "jimp";
 
-async function resizeImg(path, size, quality) {
+async function resizeImg(path, size = 250, quality = 60) {
   Jimp.read(path, (err, lenna) => {
     if (err) throw err;
     lenna
