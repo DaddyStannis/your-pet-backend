dc854286479dacc22bd4d8265395cfa3adce3e93
Stanislav Sakun <stanislav.sakun@ukr.net>
2023-05-17 21:07:10 +0300

    Added cors options

--- app.js
+++ app.js
@@ -16,7 +16,15 @@ const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
-app.use(cors());
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.static("public"));
 
